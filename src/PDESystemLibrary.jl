39c1a752b8e84cb49d61c75cdb8a54fd13a3139c
xtalax <alex.lemony@gmail.com>
2023-02-28 16:20:30 +0000

    convection diffusion

--- src/PDESystemLibrary.jl
+++ src/PDESystemLibrary.jl
@@ -12,12 +12,14 @@ all_systems = []
 
 include("../lib/burgers.jl")
 include("../lib/linear_diffusion.jl")
+include("../lib/linear_convection.jl")
+include("../lib/nonlinear_diffusion.jl")
 include("../lib/general_linear_system.jl")
 include("../lib/brusselator.jl")
 
-function get_pdesys_with_tags(tags...)
+function get_pdesys_with_tags(tags...; f = all)
     filter(all_systems) do ex
-        all(t -> t in ex.metadata, tags)
+        f(t -> t in ex.metadata, tags)
     end
 end
 
