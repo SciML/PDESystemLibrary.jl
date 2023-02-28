12dbdb74395d2acd7b223ef9e0756e508d200fcb
xtalax <alex.lemony@gmail.com>
2023-02-28 16:19:40 +0000

    more diffusion

--- lib/linear_diffusion.jl
+++ lib/linear_diffusion.jl
@@ -34,5 +34,45 @@ function heat_1d1()
 
     heat_1d1
 end
-
 push!(all_systems, heat_1d1())
+
+"""
+# The Heat Equation in 1D with Neumann Boundary Conditions.
+
+1D heat equation with Neumann boundary conditions.
+This models the temperature of a rod over time, where the ends are held at a constant temperature.
+
+It is initialized with a sinusoidal profile.
+The equation is given by:
+
+```math
+\\frac{\\partial u}{\\partial t} = D \\frac{\\partial^2 u}{\\partial x^2}
+```
+"""
+function heat_1d_neumann()
+    # Method of Manufactured Solutions
+
+    # Parameters, variables, and derivatives
+    @parameters t x
+    @variables u(..)
+    Dt = Differential(t)
+    Dx = Differential(x)
+    Dxx = Differential(x)^2
+
+    # 1D PDE and boundary conditions
+    eq = Dt(u(t, x)) ~ Dxx(u(t, x))
+    bcs = [u(0, x) ~ cos(x),
+           Dx(u(t, 0)) ~ 0,
+           Dx(u(t, Float64(pi))) ~ 0]
+
+    # Space and time domains
+    domains = [t ∈ Interval(0.0, 1.0),
+    x ∈ Interval(0.0, Float64(pi))]
+
+    analytic = [u(t, x) ~ exp(-t) * cos(x)]
+
+    tags = ["1D", "Neumann", "Linear", "Diffusion", "Heat"]
+    # PDE system
+    @named pdesys = PDESystem(eq, bcs, domains, [t, x], [u(t, x)], analytic = analytic, metadata = tags)
+end
+push!(all_systems, heat_1d_neumann())
---
+++ lib/nonlinear_diffusion.jl
@@ -0,0 +1,40 @@
+"""
+# Spherical Laplacian 1D
+
+The laplacian in spherical coordinates, with Dirichlet and Neumann boundary conditions.
+Symmetrical in azimuth and elevation, so the solution is a function of only one spatial
+variable.
+
+```math
+\\frac{\\partial^2 u}{\\partial t^2} - \\frac{4}{r^2} \\frac{\\partial}{\\partial r}(r^2 \\frac{\\partial}{\\partial r}(u) = 0
+```
+"""
+function spherical_laplacian()
+    # Parameters, variables, and derivatives
+    @parameters t r
+    @variables u(..)
+    Dt = Differential(t)
+    Dr = Differential(r)
+
+    # 1D PDE and boundary conditions
+
+    eq = Dt(u(t, r)) ~ 4 / r^2 * Dr(r^2 * Dr(u(t, r)))
+    bcs = [u(0, r) ~ sin(r) / r,
+           Dr(u(t, 0)) ~ 0,
+           u(t, 1) ~ exp(-4t) * sin(1)]
+
+    # Space and time domains
+    domains = [t ∈ Interval(0.0, 1.0),
+               r ∈ Interval(0.0, 1.0)]
+
+    u_exact = [u(t, r) ~ exp.(-4t) * sin.(r) ./ r]
+
+    tags = ["1D", "Dirichlet", "Neumann", "Spherical", "Diffusion"]
+
+    # PDE system
+    @named sph = PDESystem(eq, bcs, domains, [t, r], [u(t, r)], analytic = u_exact, metadata = tags)
+
+    sph
+end
+
+push!(all_systems, spherical_laplacian())
