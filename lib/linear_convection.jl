141d89b1375fc572e210acf5050df02d27566599
xtalax <alex.lemony@gmail.com>
2023-03-01 16:29:18 +0000

    ps for covection

--- lib/linear_convection.jl
+++ lib/linear_convection.jl
@@ -11,52 +11,53 @@ It can take any initial condition profile, as long as it is periodic.
 \\frac{\\partial u}{\\partial t} + c \\frac{\\partial u}{\\partial x} = 0
 ```
 """
-function linear_convection(f, name = :linear_convection)
-    @variables x t u(..)
+function linear_convection(f, ps, name = :linear_convection)
+    @variables x t v u(..)
     Dt = Differential(t)
     Dx = Differential(x)
 
     # 1D PDE and boundary conditions
-    eq = Dt(u(t, x)) ~ - Dx(u(t, x))
+    eq = Dt(u(t, x)) ~ -v * Dx(u(t, x))
 
     @register_symbolic f(x)
 
     bcs = [u(0, x) ~ f(x),
-           u(t, 0) ~ u(t, 1)]
+        u(t, 0) ~ u(t, 1)]
     # Space and time domains
     domains = [t ∈ Interval(0.0, 10.0),
-               x ∈ Interval(0.0, 1.0)]
+        x ∈ Interval(0.0, 1.0)]
 
     # Analytic solution
-    u_exact = [u(t, x) ~ f(x - t)]
+    u_exact = [u(t, x) ~ f(x - v * t)]
 
     tags = ["1D", "Periodic", "Linear", "Advection"]
 
     # PDE system
-    lin_conv = PDESystem(eq, bcs, domains, [t, x], [u(t, x)], analytic = u_exact, metadata = tags, name = name)
+    lin_conv = PDESystem(eq, bcs, domains, [t, x], [u(t, x)], [v => ps[1]],
+                         analytic = u_exact, metadata = tags, name = name)
 
     lin_conv
 end
 
 # sinusoidal input
-convsin = linear_convection(x -> sin(2pi * x), :convsin)
+convsin = linear_convection(x -> sin(2pi * x), [1], :convsin)
 push!(convsin.metadata, "Sinusoidal")
 push!(all_systems, convsin)
 
 #cosine input
-convcos = linear_convection(x -> cos(2pi * x), :convcos)
+convcos = linear_convection(x -> cos(2pi * x), [-2], :convcos)
 push!(convcos.metadata, "Sinusoidal")
 push!(all_systems, convcos)
 
 # triangular input
-convtri = linear_convection(x -> 1.0 - abs(x - floor(x + 0.5)), :convtri)
+convtri = linear_convection(x -> 1.0 - abs(x - floor(x + 0.5)), [0.6], :convtri)
 push!(convtri.metadata, "Triangular")
 push!(all_systems, convtri)
 
 # square wave
 f = (x) -> ifelse(x - floor(x) < 0.5, 1.0, -1.0)
 
-convsquare = linear_convection(f, :convsquare)
+convsquare = linear_convection(f, [-1.1], :convsquare)
 push!(convsquare.metadata, "Square")
 push!(convsquare.metadata, "Discontinuous")
 push!(all_systems, convsquare)
@@ -92,30 +93,32 @@ function convection_diffusion(L, ps, name = :convection_diffusion)
     @register_symbolic f_0(x)
 
     bcs = [f(0, x) ~ f_0(x),
-           f(t, 0) ~ 0.0, f(t, L) ~ 1.0]
+        f(t, 0) ~ 0.0, f(t, L) ~ 1.0]
     # Space and time domains
     domains = [t ∈ Interval(0.0, 30.0),
-               x ∈ Interval(0.0, L)]
+        x ∈ Interval(0.0, L)]
 
     # Analytic/reference solution
-    λ(n) = (n*π/L)^2
+    λ(n) = (n * π / L)^2
 
     maxiters = 1000
 
     function A(ps, t, z)
         k = ps[1]
         v = ps[2]
-        exp(-((t*v^2)/(4k) + (v*z)/(2k)))
+        exp(-((t * v^2) / (4k) + (v * z) / (2k)))
     end
 
     function u(ps, t, z)
         k = ps[1]
         v = ps[2]
-        a = z/L * exp((t*v^2)/(4k) + (v*L)/(2k))
+        a = z / L * exp((t * v^2) / (4k) + (v * L) / (2k))
         s = mapreduce((+), 1:maxiters) do n
-            acu = (2 * (-1)^n *v^2 * exp(-k*λ(n)*t + (v*L)/(2k))*(exp(k*λ(n)*t + (t*v^2)/(4k)) - 1)) / (n*π * (4*k^2*λ(n) + v^2))
-            acu += 2/L*((-1)^n)*exp(v*L/(2k))*exp(-k*λ(n)*t)/sqrt(λ(n))
-            acu *= sin(λ(n)*z)
+            acu = (2 * (-1)^n * v^2 * exp(-k * λ(n) * t + (v * L) / (2k)) *
+                   (exp(k * λ(n) * t + (t * v^2) / (4k)) - 1)) /
+                  (n * π * (4 * k^2 * λ(n) + v^2))
+            acu += 2 / L * ((-1)^n) * exp(v * L / (2k)) * exp(-k * λ(n) * t) / sqrt(λ(n))
+            acu *= sin(λ(n) * z)
 
             acu
         end
@@ -127,7 +130,9 @@ function convection_diffusion(L, ps, name = :convection_diffusion)
     tags = ["1D", "Dirichlet", "Advection", "Diffusion", "Monotonic"]
 
     # PDE system
-    convdiff = PDESystem(eq, bcs, domains, [t, x], [f(t, x)], [k => ps[1], v => ps[2]] analytic = ref, metadata = tags, name = name)
+    convdiff = PDESystem(eq, bcs, domains, [t, x], [f(t, x)],
+                         [k => ps[1], v => ps[2]]analytic = ref, metadata = tags,
+                         name = name)
 
     convdiff
 end
